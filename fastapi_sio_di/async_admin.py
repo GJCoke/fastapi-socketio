d6544a6d62f68a79cdc71d5410095bc1ec5ea4f1
coke <v-wangtao45@xiaomi.com>
2026-01-07 19:18:05 +0800

    fix(admin): add async admin_disconnect method and update version to 0.3.2

--- fastapi_sio_di/async_admin.py
+++ fastapi_sio_di/async_admin.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 from socketio.async_admin import (
     InstrumentedAsyncServer as SocketIOInstrumentedAsyncServer,
 )
@@ -10,3 +10,6 @@ class InstrumentedAsyncServer(SocketIOInstrumentedAsyncServer):
 
     async def admin_connect(self, sid: SID, environ: Environ, client_auth: Any):
         return await super().admin_connect(sid=sid, environ=environ, client_auth=client_auth)
+
+    async def admin_disconnect(self, sid: SID, namespace: str, close: bool, room_filter: List[str] = None):
+        return await super().admin_disconnect(sid, namespace=namespace, close=close, room_filter=room_filter)
